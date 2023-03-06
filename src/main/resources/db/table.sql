719105fe43d1f0a5a4b9b667e92e6f6eb590e4d1
aozp73 <aozp73@naver.com>
2023-03-06 12:04:48 +0900

    스크랩, 좋아요 기능 - table, mapper, model 생성 완료(주말 다른 곳에서 작업 후 생성)

--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -78,3 +78,10 @@ create table custom_board_tb(
     content varchar not null,
     created_at timestamp not null
 );
+
+create table love_tb(
+    id int auto_increment primary key,
+    board_id int not null,
+    user_id int not null,
+    created_at timestamp not null
+);
