e14a3b230861ee4e0d986d2521939d4c3cd83122
kimalss22 <kimalss22@gmail.com>
2023-03-07 13:34:12 +0900

    불필요한 주석 제거

--- src/main/java/shop/mtcoding/jobara/common/aop/AopHandler.java
+++ src/main/java/shop/mtcoding/jobara/common/aop/AopHandler.java
@@ -1,7 +1,5 @@
 package shop.mtcoding.jobara.common.aop;
 
-import javax.servlet.http.HttpSession;
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -9,7 +7,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.ui.Model;
 
 import shop.mtcoding.jobara.common.util.RedisService;
 import shop.mtcoding.jobara.common.util.Verify;
@@ -21,26 +18,13 @@ public class AopHandler {
     @Autowired
     private RedisService redisService;
 
-    @Autowired
-    private HttpSession session;
-
-    // @Autowired
-    // private ObjectMapper om;
-
     @Pointcut("@annotation(shop.mtcoding.jobara.common.aop.CompanyCheck)")
     public void CompanyCheck() {
     }
 
     @Before("CompanyCheck()")
     public void CompanyCheck(JoinPoint joinPoint) {
-        // String principalJson =
         UserVo principal = redisService.getValue("principal");
-        // try {
-        // principal = om.readValue(principalJson, UserVo.class);
-        // } catch (Exception e) {
-        // System.out.println("파싱 실패");
-        // }
-        // UserVo principal = redisService.getValue("principal");
         Verify.validateObject(principal, "로그인이 필요한 기능입니다", HttpStatus.UNAUTHORIZED, "/#login");
         Verify.checkRole(principal, "company");
     }
@@ -51,8 +35,6 @@ public class AopHandler {
 
     @Before("CompanyCheckApi()")
     public void CompanyCheckApi(JoinPoint joinPoint) {
-        // UserVo principal = (UserVo) redisTemplate.opsForValue().get("principal");
-        // UserVo principal = redisService.getValue("principal");
         UserVo principal = redisService.getValue("principal");
         Verify.validateApiObject(principal, "로그인이 필요한 기능입니다", HttpStatus.UNAUTHORIZED);
         Verify.checkRoleApi(principal, "company");
@@ -64,8 +46,6 @@ public class AopHandler {
 
     @Before("EmployeeCheck()")
     public void EmployeeCheck(JoinPoint joinPoint) {
-        // UserVo principal = (UserVo) redisTemplate.opsForValue().get("principal");
-        // UserVo principal = redisService.getValue("principal");
         UserVo principal = redisService.getValue("principal");
         Verify.validateObject(principal, "로그인이 필요한 기능입니다", HttpStatus.UNAUTHORIZED, "/#login");
         Verify.checkRole(principal, "employee");
@@ -77,8 +57,6 @@ public class AopHandler {
 
     @Before("EmployeeCheckApi()")
     public void EmployeeCheckApi(JoinPoint joinPoint) {
-        // UserVo principal = (UserVo) redisTemplate.opsForValue().get("principal");
-        // UserVo principal = redisService.getValue("principal");
         UserVo principal = redisService.getValue("principal");
         Verify.validateApiObject(principal, "로그인이 필요한 기능입니다", HttpStatus.UNAUTHORIZED);
         Verify.checkRoleApi(principal, "employee");
